7df155c005d3ad3c524f9fdcbc9af707399219f8
Daze <44749931+errleng@users.noreply.github.com>
2020-03-23 19:45:58 -0400

    Add project files.

---
+++ Pomodoro/MainWindow.xaml.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Pomodoro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // constants
+        private const int MINUTE = 60; // seconds in one minute
+        private const int HOUR = 60 * MINUTE; // seconds in one hour
+        private const string POMODORO_STATUS = "Pomodoro";
+        private const string LONG_BREAK_STATUS = "Long break";
+        private const string SHORT_BREAK_STATUS = "Short break";
+        private const string DEFAULT_POMODORO_SOUND_FILE = "pomodoro-sound";
+        private const string DEFAULT_BREAK_SOUND_FILE = "break-sound";
+
+        // configuration constants
+        private DispatcherTimer timer;
+        private TimeSpan remainingTime;
+        private int completedPomodoros;
+
+        // instance variables
+        MediaPlayer mediaPlayer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // initialize instance variables
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.Volume = 1.0;
+            mediaPlayer.MediaEnded += new EventHandler(Media_Ended);
+
+            // initialize time
+            remainingTime = TimeSpan.FromMinutes(Properties.Settings.Default.PomodoroDuration);
+            StatusLabel.Content = POMODORO_STATUS;
+
+            // initialize timer
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(DispatcherTimer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+
+            // check if files are chosen
+            if (!File.Exists(Properties.Settings.Default.PomodoroSoundFile) || !File.Exists(Properties.Settings.Default.BreakSoundFile))
+            {
+                SetDefaultSettings();
+            }
+        }
+
+        public static void SetDefaultSettings()
+        {
+            // set settings to default values
+
+            // find files by default name
+            DirectoryInfo currentDirectory = new DirectoryInfo(Directory.GetCurrentDirectory() + "/assets");
+            FileInfo[] files = currentDirectory.GetFiles($"{DEFAULT_POMODORO_SOUND_FILE}.*");
+            if (files.Length > 0)
+            {
+                Properties.Settings.Default.PomodoroSoundFile = files[0].FullName;
+            }
+            files = currentDirectory.GetFiles($"{DEFAULT_BREAK_SOUND_FILE}.*");
+            if (files.Length > 0)
+            {
+                Properties.Settings.Default.BreakSoundFile = files[0].FullName;
+            }
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (remainingTime == TimeSpan.Zero)
+            {
+                TimerLabel.Content = $"{remainingTime}";
+
+                // transition to next state
+                if (StatusLabel.Content.Equals(POMODORO_STATUS))
+                {
+                    completedPomodoros += 1;
+                    PomodoroCounterLabel.Content = $"{completedPomodoros} Pomodoros";
+                    if ((completedPomodoros > 0) && (completedPomodoros % Properties.Settings.Default.LongBreakPomodoros == 0))
+                    {
+                        // start long break
+                        StatusLabel.Content = LONG_BREAK_STATUS;
+                        remainingTime = TimeSpan.FromMinutes(Properties.Settings.Default.LongBreakDuration);
+                    }
+                    else
+                    {
+                        // start short break
+                        StatusLabel.Content = SHORT_BREAK_STATUS;
+                        remainingTime = TimeSpan.FromMinutes(Properties.Settings.Default.ShortBreakDuration);
+                    }
+
+                    // completion message and sound
+                    pomodoroCompletionMessage();
+                }
+                else
+                {
+                    // break complete
+                    // start a Pomodoro
+                    StatusLabel.Content = POMODORO_STATUS;
+                    remainingTime = TimeSpan.FromMinutes(Properties.Settings.Default.PomodoroDuration);
+
+                    breakCompletionMessage();
+                }
+
+                TimerLabel.Content = $"{remainingTime}";
+                ToggleTimer();
+            }
+            else
+            {
+                TimerLabel.Content = $"{remainingTime}";
+                remainingTime = remainingTime.Subtract(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private void ToggleTimer()
+        {
+            if (!timer.IsEnabled)
+            {
+                StartTimerButton.Content = "Stop";
+                SkipButton.Visibility = Visibility.Visible;
+                timer.Start();
+            }
+            else
+            {
+                StartTimerButton.Content = "Start";
+                SkipButton.Visibility = Visibility.Hidden;
+                timer.Stop();
+            }
+        }
+
+        private void pomodoroCompletionMessage()
+        {
+            if (!File.Exists(Properties.Settings.Default.PomodoroSoundFile))
+            {
+                MessageBox.Show("Pomodoro completion sound file not found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // start playing sound
+            mediaPlayer.Open(new Uri(Properties.Settings.Default.PomodoroSoundFile));
+            mediaPlayer.Play();
+
+            // show message
+            string title = "Pomodoro Complete";
+            string text = "Pomodoro complete!\nContinue to next state?";
+            if (MessageBox.Show(text, title, MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                ToggleTimer();
+            }
+
+            // stop playing sound after message confirmed received
+            mediaPlayer.Stop();
+        }
+
+        private void breakCompletionMessage()
+        {
+            if (!File.Exists(Properties.Settings.Default.BreakSoundFile))
+            {
+                MessageBox.Show("Break completion sound file not found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // start playing sound
+            mediaPlayer.Open(new Uri(Properties.Settings.Default.BreakSoundFile));
+            mediaPlayer.Play();
+
+            // show message
+            string title = "Break Complete";
+            string text = "Break complete!\nContinue to next state?";
+            if (MessageBox.Show(text, title, MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                ToggleTimer();
+            }
+
+            // stop playing sound after message confirmed received
+            mediaPlayer.Stop();
+        }
+
+        private void StartTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleTimer();
+        }
+
+        private void SkipButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (timer.IsEnabled)
+            {
+                remainingTime = TimeSpan.Zero;
+            }
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow settings = new SettingsWindow();
+            settings.Show();
+        }
+
+        private void Media_Ended(object sender, EventArgs e)
+        {
+            mediaPlayer.Position = TimeSpan.Zero;
+            mediaPlayer.Play();
+        }
+    }
+}
---
+++ Pomodoro/SettingsWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace Pomodoro
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+            // update text boxes with file names
+            PomodoroSoundTextBox.Text = Path.GetFileName(Properties.Settings.Default.PomodoroSoundFile);
+            BreakSoundTextBox.Text = Path.GetFileName(Properties.Settings.Default.BreakSoundFile);
+            PomodoroDurationTextBox.Text = Properties.Settings.Default.PomodoroDuration.ToString();
+            ShortBreakDurationTextBox.Text = Properties.Settings.Default.ShortBreakDuration.ToString();
+            LongBreakDurationTextBox.Text = Properties.Settings.Default.LongBreakDuration.ToString();
+            LongBreakPomodorosTextBox.Text = Properties.Settings.Default.LongBreakPomodoros.ToString();
+        }
+
+        private void SelectPomodoroSoundButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == true)
+            {
+                // set setting
+                Properties.Settings.Default.PomodoroSoundFile = fileDialog.FileName;
+                Properties.Settings.Default.Save();
+
+                // update text box with choice
+                PomodoroSoundTextBox.Text = fileDialog.SafeFileName;
+            }
+        }
+
+        private void SelectBreakSoundButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == true)
+            {
+                // set setting
+                Properties.Settings.Default.BreakSoundFile = fileDialog.FileName;
+                Properties.Settings.Default.Save();
+
+                // update text box with choice
+                BreakSoundTextBox.Text = fileDialog.SafeFileName;
+            }
+        }
+
+        private void PomodoroDurationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(PomodoroDurationTextBox.Text, out int duration))
+            {
+                if (duration > 0)
+                {
+                    Properties.Settings.Default.PomodoroDuration = duration;
+                    return;
+                }
+            }
+            MessageBox.Show("Input must be a positive integer of minutes", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void ShortBreakDurationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(ShortBreakDurationTextBox.Text, out int duration))
+            {
+                if (duration > 0)
+                {
+                    Properties.Settings.Default.ShortBreakDuration = duration;
+                    return;
+                }
+            }
+            MessageBox.Show("Input must be a positive integer of minutes", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void LongBreakDurationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(LongBreakDurationTextBox.Text, out int duration))
+            {
+                if (duration > 0)
+                {
+                    Properties.Settings.Default.LongBreakDuration = duration;
+                    return;
+                }
+            }
+            MessageBox.Show("Input must be a positive integer of minutes", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void LongBreakPomodorosButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(LongBreakPomodorosTextBox.Text, out int numPomodoros))
+            {
+                if (numPomodoros > 0)
+                {
+                    Properties.Settings.Default.LongBreakDuration = numPomodoros;
+                    return;
+                }
+            }
+            MessageBox.Show("Input must be a positive integer", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Reset();
+
+            // cannot only use Properties.Settings.Default.Reset() because defaults are dynamically assigned
+            MainWindow.SetDefaultSettings();
+
+            // update text boxes with choice
+            PomodoroSoundTextBox.Text = Path.GetFileName(Properties.Settings.Default.PomodoroSoundFile);
+            BreakSoundTextBox.Text = Path.GetFileName(Properties.Settings.Default.BreakSoundFile);
+
+            Properties.Settings.Default.Save();
+        }
+    }
+}
